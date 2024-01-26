488cf7df8e259d0d8c58569ce7eb7e2d3e611df5
jdufresne12 <119969698+jdufresne12@users.noreply.github.com>
2024-01-26 14:08:11 -0500

    Update assignment1.cpp

--- assignment1.cpp
+++ assignment1.cpp
@@ -32,7 +32,7 @@ bool isPrime(int num) {
 void findPrimes(int start, int end) {
     int primeCount = 0;
     long long primeSum = 0;
-    for (int i = start; i <= end; i++) {
+    for (int i = start; i <= end; i+=8) {
         if (isPrime(i)) {
             primeCount++;
             primeSum += i;
@@ -51,11 +51,8 @@ int main() {
     //start timer
     auto start_timer = chrono::high_resolution_clock::now(); 
 
-    //Determine range for threads to process
-    for (int i = 0; i < NUM_THREADS; ++i) {
-        int start = (MAX / NUM_THREADS) * i + 1; //Grabbing start value for threads loop
-        int end = (i == NUM_THREADS - 1) ? MAX : (MAX / NUM_THREADS) * (i + 1); //Grabbing end value for threads loop, if its the end, end=MAX
-        threads.push_back(thread(findPrimes, start, end)); //Create thread and push to back of vector 
+    for(int i = 1; i <= NUM_THREADS; ++i){
+        threads.push_back(thread(findPrimes, i, MAX));
     }
 
     //Join threads 
